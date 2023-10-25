98732afab987321ec3fa55de21ede84e93e9a6c2
zhandos <zhandosshakimurat@gmail.com>
2023-10-25 14:17:52 +0600

    unittest refactor

---
+++ test/test_battery/test_nubbin_battery.py
@@ -0,0 +1,19 @@
+import unittest
+from datetime import datetime
+from battery.nubbin_battery import NubbinBattery
+
+
+class TestNubbinBattery(unittest.TestCase):
+    def test_battery_should_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 5)
+        battery = NubbinBattery(today, last_service_date)
+
+        self.assertTrue(battery.needs_service())
+
+    def test_battery_should_not_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 3)
+
+        car = NubbinBattery(today, last_service_date)
+        self.assertFalse(car.needs_service())
---
+++ test/test_battery/test_spindler_battery.py
@@ -0,0 +1,19 @@
+import unittest
+from datetime import datetime
+from battery.spindler_battery import SpindlerBattery
+
+
+class TestSpindlerBattery(unittest.TestCase):
+    def test_battery_should_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 3)
+        battery = SpindlerBattery(today, last_service_date)
+
+        self.assertTrue(battery.needs_service())
+
+    def test_battery_should_not_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 1)
+
+        car = SpindlerBattery(today, last_service_date)
+        self.assertFalse(car.needs_service())
